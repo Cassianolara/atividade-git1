98c00a782c722e142086c5ac4c01948378be91b7
Cassiano <cassianolara0@gmail.com>
2026-04-15 18:31:41 -0300

    novo ajuste

--- atividade.js
+++ atividade.js
@@ -3,15 +3,16 @@ const rl = readline.createInterface({
     input : process.stdin,
     output : process.stdout
 })
-rl.question('Voce gosta de cafe?', (input)=>{
+rl.question('Você gosta de café?', (input)=>{
     const cafe = input
     if(cafe === 'sim'){
-        console.log('Café traz muitos beneficios')
+        console.log('Café traz muitos benefícios')
         rl.close()
     }else{
-        if(cafe === 'nao' || cafe === 'não'){
-        console.log('Muitas pessoas nao gostam de cafe')
+        if(cafe === 'nao' || cafe === 'não' || cafe === 'Não' || cafe === 'Nao'){
+        console.log('Muitas pessoas não gostam de café')
         rl.close()
     }
 }
 })
+// ajuste de gramatica
